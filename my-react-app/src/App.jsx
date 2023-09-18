408e98e5efea04a33110b079e428c0cd97b6c742
syauqidamario <syauqi.djohan@student.umn.ac.id>
2023-09-18 18:26:43 +0700

    First commits. Project setup.

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+
+  return (
+  <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+    <div>
+      Hello There!
+    </div>
+  </div>
+  );
+}
+
+export default App;
