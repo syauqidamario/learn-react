5bbd1b013515f950dbbf4328c14228467c8fa8f7
syauqidamario <syauqi.djohan@student.umn.ac.id>
2023-09-23 17:05:35 +0700

    Added stage management using Redux.

---
+++ my-react-app/redux.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+// reducer
+
+import { legacy_createStore } from "redux";
+
+const cartReducer = (
+
+      state = {
+            login: false,
+            cart:[{id:1, qty:20}],
+      },
+      action 
+) => {
+      switch(action.type){
+            case "ADD_TO_CART":
+                  return{
+                        ...state,
+                        cart: [...state.cart, action.payload],
+                  };
+            default:
+                  return state;
+      }
+};
+
+// store
+const store = legacy_createStore(cartReducer);
+console.log("oncreate store: ", store.getState());
+
+// subscribe
+store.subscribe(() => {
+      console.log("STORE CHANGE:", store.getState());
+});
+
+//dispatch
+const action1 = { type: "ADD_TO_CART", payload: {id: 2, qty: 20} };
+store.dispatch(action1);
+const action2 = { type: "ADD_TO_CART", payload: {id: 10, qty: 40} };
+store.dispatch(action2);
