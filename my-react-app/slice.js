818cc311313ad6506dcb702c9aff3b3c84e1c8ba
syauqidamario <syauqi.djohan@student.umn.ac.id>
2023-09-23 21:44:31 +0700

    Added example of Redux toolkit usage.

---
+++ my-react-app/slice.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+import toolkit from "@reduxjs/toolkit";
+
+const { configureStore, createSlice } = toolkit;
+
+const cartSlice = createSlice({
+      name: "cart",
+      initialState: [],
+      reducers:{
+            addToCart(state, action){
+                  state.push(action.payload);
+            },
+      },
+});
+
+const store = configureStore({
+      reducer: {
+            cart: cartSlice.reducer,
+      },
+});
+
+store.subscribe(() => {
+      console.log("STORE CHANGE:", store.getState());
+});
+
+store.dispatch(cartSlice.actions.addToCart({id: 1, qty:20}));
---
+++ my-react-app/toolkit.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+import toolkit from "@reduxjs/toolkit";
+
+// eslint-disable-next-line no-unused-vars
+const { configureStore, createAction, createReducer } = toolkit;
+
+const addToCart = createAction("ADD_TO_CART");
+
+
+// eslint-disable-next-line no-unused-vars
+const cartReducer = createReducer([], (builder) => {
+      builder.addCase(addToCart, (state, action) => {
+            state.push(action.payload);
+      });
+});
+
+const login = createAction("CREATE_SESSION");
+
+const loginReducer = createReducer({status:false}, (builder) => {
+      builder.addCase(login, (state, action) => {
+            state.status = true;
+      });
+});
+
+const store = configureStore({
+      reducer: {
+            login: loginReducer,
+            cart: cartReducer,
+      },
+});
+console.log("oncreate store : ", store.getState());
+
+store.subscribe(() => {
+      console.log("STORE CHANGE:", store.getState());
+});
+
+store.dispatch(addToCart({id:1, qty:20}));
+store.dispatch(addToCart({id:2, qty:10}));
+store.dispatch(login());
